2ebc8fdb7c0c9b315ac30b46b18ee07addd6847a
George Louis O. Dela Cruz <louis.gatessoft@gmail.com>
2017-08-06 22:14:35 -0700

    Modified Button

--- Section2Tutorial/app/src/main/java/com/example/georg/section2tutorial/MainActivity.java
+++ Section2Tutorial/app/src/main/java/com/example/georg/section2tutorial/MainActivity.java
@@ -7,17 +7,17 @@ import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button;
+    Button button4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       // button = (Button) findViewById(R.id.button);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Toast.makeText(getApplicationContext(),"Hello World!",Toast.LENGTH_LONG).show();
-//            }
-//        });
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Hello World!",Toast.LENGTH_LONG).show();
+            }
+        });
     }
     }
