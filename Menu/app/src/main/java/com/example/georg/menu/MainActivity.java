9394f4a94b3148d7e2e4e343336a2bb30b927249
George Louis O. Dela Cruz <louis.gatessoft@gmail.com>
2017-08-09 20:56:26 -0700

    Fixed Searchview

--- Menu/app/src/main/java/com/example/georg/menu/MainActivity.java
+++ Menu/app/src/main/java/com/example/georg/menu/MainActivity.java
@@ -4,7 +4,7 @@ import android.app.SearchManager;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SearchView;
+import android.widget.SearchView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,39 +21,39 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.mainmenu,menu);
-        SearchView sv = (SearchView) menu.findItem(R.id.searchbar).getActionView();
+        SearchView sv = (SearchView) menu.findItem(R.id.searchbarr).getActionView();
         SearchManager sm = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         sv.setSearchableInfo(sm.getSearchableInfo(getComponentName()));
         sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-             //   Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),query,Toast.LENGTH_SHORT).show();
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                //Toast.makeText(getApplicationContext(),newText,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(),newText,Toast.LENGTH_SHORT).show();
                 return false;
             }
         });
         return true;
     }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        switch(item.getItemId())
-        {
-            case R.id.Add:
-                Toast.makeText(getApplicationContext(),"You clicked on Add!",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.Settings:
-                Toast.makeText(getApplicationContext(),"You clicked on Settings!",Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.Home:
-                Toast.makeText(getApplicationContext(),"You clicked on Home!",Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        switch(item.getItemId())
+//        {
+//            case R.id.Add:
+//                Toast.makeText(getApplicationContext(),"You clicked on Add!",Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.Settings:
+//                Toast.makeText(getApplicationContext(),"You clicked on Settings!",Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.Home:
+//                Toast.makeText(getApplicationContext(),"You clicked on Home!",Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
